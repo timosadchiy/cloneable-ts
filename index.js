c2494c62d25ccdd094498bbfa9af67e09646eddc
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2017-11-29 20:03:47 +0000

    index.js for stricter tsconfig policy

---
+++ index.js
@@ -0,0 +1,69 @@
+"use strict";
+
+var Cloneable = /** @class */ (function () {
+    function Cloneable(args) {
+        applyArgs(this, args);
+    }
+
+    Cloneable.prototype.clone = function (args) {
+        return cloneToArgs(this, args || {});
+    };
+    return Cloneable;
+}());
+exports.Cloneable = Cloneable;
+
+function applyArgs(obj, args) {
+    for (var key in args) {
+        if (args.hasOwnProperty(key)) {
+            obj[key] = args[key];
+        }
+    }
+}
+
+function deepClone(oldObj) {
+    var newObj;
+    // Handle the 3 simple types, and null or undefined
+    if (null == oldObj || "object" !== typeof oldObj) {
+        return oldObj;
+    }
+    // Handle Date
+    if (oldObj instanceof Date) {
+        newObj = new Date();
+        newObj.setTime(oldObj.getTime());
+        return newObj;
+    }
+    // Handle Array
+    if (oldObj instanceof Array) {
+        newObj = [];
+        for (var i = 0, len = oldObj.length; i < len; i++) {
+            newObj[i] = deepClone(oldObj[i]);
+        }
+        return newObj;
+    }
+    // Handle Object
+    if (oldObj instanceof Object) {
+        newObj = Object.create(oldObj);
+        for (var attr in oldObj) {
+            if (oldObj.hasOwnProperty(attr)) {
+                newObj[attr] = deepClone(oldObj[attr]);
+            }
+        }
+        return newObj;
+    }
+    throw new Error("Unable to copy obj! Its type isn't supported.");
+}
+
+function cloneToArgs(originalObj, cloneArgs) {
+    var constructedObj = Object.create(originalObj);
+    for (var i in originalObj) {
+        if (originalObj.hasOwnProperty(i)) {
+            if (cloneArgs[i] != null) {
+                constructedObj[i] = deepClone(cloneArgs[i]);
+            }
+            else {
+                constructedObj[i] = deepClone(originalObj[i]);
+            }
+        }
+    }
+    return constructedObj;
+}
